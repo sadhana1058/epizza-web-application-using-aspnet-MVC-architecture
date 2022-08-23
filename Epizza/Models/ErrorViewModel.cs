6e7fa53a1fd219f6d882bf33781dc86195f524d2
Calypso11 <calypsothreemusketeers@gmail.com>
2022-08-23 18:16:05 +0530

    Add project files.

---
+++ Epizza/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Epizza.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
